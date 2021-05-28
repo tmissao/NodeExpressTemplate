bb0be2f52806b64ee3cdf36e50f43fe8cecca42b
Missão <tiago.missao@acqio.com.br>
2021-05-28 15:10:34 +0000

    docker-build

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -2,14 +2,24 @@
 
 set -euo pipefail
 
+      DOCKER_IMAGE: "${DOCKER_IMAGE}"
+      DOCKER_IMAGE_REGISTRY_KEY: "${DOCKER_IMAGE_REGISTRY_KEY}"
+      DOCKER_IMAGE_NAME_KEY: "${DOCKER_IMAGE_NAME_KEY}"
+      DOCKER_IMAGE_TAG_KEY: "${DOCKER_IMAGE_TAG_KEY}"
+
 if [[ -z "$DOCKER_IMAGE" ]]; then
     echo "DOCKER_IMAGE EMPTY"
-    DOCKER_IMAGE=$(buildkite-agent meta-data get $DOCKER_IMAGE_KEY)
-    if [[ -z "$DOCKER_IMAGE" ]]; then
-        echo "DOCKER_IMAGE KEY EMPTY"
-        echo "DOCKER_IMAGE is REQUIRED"
+
+    REGISTRY=$(buildkite-agent meta-data get $DOCKER_IMAGE_REGISTRY_KEY)
+    IMAGE=$(buildkite-agent meta-data get $DOCKER_IMAGE_NAME_KEY)
+    TAG=$(buildkite-agent meta-data get $DOCKER_IMAGE_TAG_KEY)
+
+    if [[ -z "$REGISTRY" || -z "$IMAGE" || -z "$TAG" ]]; then
+        echo "Wrong Docker Image: $REGISTRY/$IMAGE:$TAG"
         exit -1
     fi
+
+    DOCKER_IMAGE="$REGISTRY/$IMAGE:$TAG"
 fi
 
 echo "Releasing image: $DOCKER_IMAGE"
